036b864bfc918a7d28d0555c2770fc912e5fa7a5
sabra-ing <benfadhelsabra592@gmail.com>
2025-08-09 15:27:42 +0100

    weather service

---
+++ app_weather/src/app/data/services/weather.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { WeatherData } from 'src/app/models/weather.model'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private baseUrl = 'http://localhost:8000/api/weather'; 
+
+  constructor(private http: HttpClient) {}
+  getWeather(governorate: string, date: string): Observable<WeatherData> {
+    return this.http.get<WeatherData>(${this.baseUrl}?governorate=${governorate}&date=${date});
+}
+}
